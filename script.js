d05e4e2e502c0807594c591753074d83eb827d37
Narresx <92748159+Narresx@users.noreply.github.com>
2022-01-10 14:39:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('JS OK');
+
+// Esercizio 1: Mail.
+/*
+1. Creare un array di email;
+2. Chiedere all’utente la sua email;
+3. Controllare che sia nella lista di chi può accedere, stampare un messaggio appropriato in console sull’esito del controllo;
+*/
+
+// Esercizio 2: Gioco dei dadi.
+/*
+1. Generare un numero random da 1 a 6, sia per il giocatore sia per il computer;
+2. Stabilire il vincitore, in base a chi fa il punteggio più alto;
+3. Stampare in console i tiri di dado e il risultato;
+*/
+
+// Svolgimento esercizio 1
